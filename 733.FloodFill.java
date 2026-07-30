a2e7df7fe1dbcc4557daf4d3480199030843a760
diwakar12138 <diwakargautam823@gmail.com>
2026-07-30 18:20:57 +0530

    Code uploaded

---
+++ 733.FloodFill.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+        int originalColor = image[sr][sc];
+
+        if (originalColor == color)
+            return image;
+
+        dfs(image, sr, sc, originalColor, color);
+
+        return image;
+    }
+
+    private void dfs(int[][] image, int row, int col, int originalColor, int newColor) {
+
+        int m = image.length;
+        int n = image[0].length;
+
+        if (row < 0 || row >= m || col < 0 || col >= n)
+            return;
+
+        if (image[row][col] != originalColor)
+            return;
+
+        image[row][col] = newColor;
+
+        dfs(image, row - 1, col, originalColor, newColor);
+        dfs(image, row + 1, col, originalColor, newColor); 
+        dfs(image, row, col - 1, originalColor, newColor); 
+        dfs(image, row, col + 1, originalColor, newColor); 
+    }
+}
